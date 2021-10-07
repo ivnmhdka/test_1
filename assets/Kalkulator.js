a9647237250c4738d133db4ecf68f268d25c87a5
ivnmhdka <ivanmahadika05@gmail.com>
2021-10-07 14:37:10 +0700

    first commit

--- assets/Kalkulator.js
+++ assets/Kalkulator.js
@@ -5,29 +5,84 @@ const calculator = {
     waitingForSecondNumber: false
 };
 
-function updateDisplay() {
+function updateDisplay(){
     document.querySelector("#displayNumber").innerText = calculator.displayNumber;
 }
 
 function clearCalculator(){
-    calculator.displayNumber = 'o';
+    calculator.displayNumber = '0';
     calculator.operator = null;
     calculator.firstNumber = null;
     calculator.waitingForSecondNumber = false;
 }
 
-function inputDigit(digit) {
-    calculator.displayNumber += digit;
+function inputDigit(digit){
+    if (calculator.displayNumber === "0"){
+        calculator.displayNumber = digit
+    }else{
+        calculator.displayNumber += digit;
+    }
+}
+
+function inverseNumber(){
+    if(calculator.displayNumber === '0'){
+        return;
+    }
+    calculator.displayNumber = calculator.displayNumber * -1;
+}
+
+function heandleOperator(operator){
+    if(!calculator.waitingForSecondNumber){
+        calculator.operator = operator;
+        calculator.waitingForSecondNumber = true;
+        calculator.firstNumber = calculator.displayNumber;
+        calculator.displayNumber = '0';
+    }else{
+        alert("Operator sudah ditetapkan")
+    }
+}
+
+function performCalculation(){
+    if(calculator.firstNumber == null || calculator.operator == null){
+        alert("Anda belum menetapkan operator");
+        return;
+    }
+
+    let result = 0;
+    if(calculator.operator === "+"){
+        result = parseInt(calculator.firstNumber) + parseInt(calculator.displayNumber);
+    }else{
+        result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber);
+    }
+    calculator.displayNumber = result;
 }
 
 const buttons = document.querySelectorAll(".button");
-for (let button of buttons) {
-    button.addEventListener('click', function(event) {
-
-        // mendapatkan objek elemen yang diklik
+for(let button of buttons){
+    button.addEventListener('click', function(event){
         const target = event.target;
+        if(target.classList.contains('clear')){
+            clearCalculator();
+            updateDisplay();
+            return;
+        }
+
+        if(target.classList.contains('negative')){
+            inverseNumber();
+            updateDisplay();
+            return;
+        }
+        if(target.classList.contains('equals')){
+            performCalculation();
+            updateDisplay();
+            return;
+        }
+        if(target.classList.contains('operator')){
+            heandleOperator(target.innerText);
+            return;
+        }
 
         inputDigit(target.innerText);
         updateDisplay()
-    });
+    })
 }
